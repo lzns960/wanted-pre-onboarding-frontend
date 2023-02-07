8a0b5804242ec79f05faf2bfe9e670dc5a94583f
lzns960 <lzns960@gmail.com>
2023-02-07 21:45:09 +0900

    feat: add todo탭기능

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -16,7 +16,7 @@ export default function TodoList({ todos, editTodo, deleteTodo }) {
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <TabList onChange={handleChangeTab}>
           <Tab label="All" value="1" />
-          <Tab label="해야 할 일" value="2" />
+          <Tab label="해야할 일" value="2" />
           <Tab label="완료한 일" value="3" />
         </TabList>
       </Box>
@@ -35,8 +35,38 @@ export default function TodoList({ todos, editTodo, deleteTodo }) {
             ))}
           </ul>
         </TabPanel>
-        <TabPanel value="2">Item Two</TabPanel>
-        <TabPanel value="3">Item Three</TabPanel>
+        <TabPanel value="2">
+          <ul>
+            {todos.map((todo, id) =>
+              todo.isCompleted === false ? (
+                <TodoItem
+                  key={id}
+                  todo={todo}
+                  index={id}
+                  editTodo={editTodo}
+                  onToggle={() => onToggle(todo.id)}
+                  deleteTodo={deleteTodo}
+                />
+              ) : null,
+            )}
+          </ul>
+        </TabPanel>
+        <TabPanel value="3">
+          <ul>
+            {todos.map((todo, id) =>
+              todo.isCompleted === true ? (
+                <TodoItem
+                  key={id}
+                  todo={todo}
+                  index={id}
+                  editTodo={editTodo}
+                  onToggle={() => onToggle(todo.id)}
+                  deleteTodo={deleteTodo}
+                />
+              ) : null,
+            )}
+          </ul>
+        </TabPanel>
       </TabWrap>
     </TabContext>
   );
